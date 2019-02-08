da5adb473da71c17311cac679e92411528eeb8d4
pratikms <pratik.shivaraikar@gmail.com>
2019-02-08 08:51:51 +0530

    Added basic layout

--- dcs/secure/urls.py
+++ dcs/secure/urls.py
@@ -3,5 +3,7 @@ from django.conf.urls import url, include
 import secure.views
 
 urlpatterns = [
-    url(r'^$', secure.views.index, name='index')
+    url(r'^$', secure.views.index, name='dashboard'),
+    url(r'^dashboard/$', secure.views.dashboard, name='dashboard'),
+    url(r'^hosts/$', secure.views.hosts, name='hosts'),
 ]
--- dcs/secure/views.py
+++ dcs/secure/views.py
@@ -4,4 +4,9 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'blank-page.html', { 'nav_active': 'blank' })
-    # return HttpResponse('<strong>Yo mama!</strong>')
+
+def dashboard(request):
+    return render(request, 'dashboard.html', { 'nav_active': 'dashboard' })
+
+def hosts(request):
+    return render(request, 'hosts.html', { 'nav_active': 'hosts' })
