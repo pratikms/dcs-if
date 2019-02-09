08a16ce4d22639c5e81e2f13c7a0a7e98838d87d
= <=>
2019-02-09 17:25:58 +0530

    Resolved merge conflict

--- dcs/secure/urls.py
+++ dcs/secure/urls.py
@@ -6,6 +6,9 @@ import secure.views as views
 urlpatterns = [
     url(r'^$', views.dashboard, name='dashboard'),
     url(r'^dashboard/$', views.dashboard, name='dashboard'),
+    url(r'^images/$', views.images, name='hosts'),
+    url(r'^comcheck/$', views.compcheck, name='hosts'),
+    path('perform/<action>/<name>', views.perform),
     url(r'^images/$', views.images, name='images'),
     path('perform/<action>/<name>', views.perform),
     path('total-images/', views.total_images)
--- dcs/secure/views.py
+++ dcs/secure/views.py
@@ -33,6 +33,91 @@ def perform(request, action='', name=''):
     filesDict['action'] = action
     return JsonResponse(filesDict)
 
+def get_output(command):
+    logger.error("Running command : " + command)
+    proc = subprocess.Popen(command,shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    output = proc.stdout.read()
+    error = proc.stderr.read()
+    if output:
+        logger.error (output)
+        return str(output.decode('utf-8').rstrip())
+    return None
+
+def compcheck(request):
+    count = 0 
+    import yaml
+    with open("definitions.yaml", 'r') as stream:
+        try:
+            results = {}
+            mydict = (yaml.load(stream))
+            command_list = []
+            for group in (mydict['groups']):
+                group_description = group['description']
+                results[group_description] = []
+                for check in group['checks']: 
+                    result_dict = {}
+                    if 'audit' in check.keys() and 'tests' in check.keys():
+                        result_dict[check['id']] = [] 
+                        logger.error(check['id'])
+                        logger.error(check['description'])
+                        temp = {}
+                        temp['description'] = check['description']
+                        #result_dict[check['id']].append({'description':check['description']})
+                        logger.error("command = " + check['audit'])
+                        flag = check['tests']['test_items'][0]['flag']
+                        test_items = check['tests']['test_items'][0]
+                        if 'compare' in test_items.keys():
+                            compare_dict = check['tests']['test_items'][0]['compare']
+                            logger.error (compare_dict)
+                            if compare_dict is not None and 'op' in compare_dict.keys() :
+                                op = compare_dict['op']
+                                value = str(compare_dict['value'])
+                                output_str = get_output(check['audit'])    
+                                if op == 'has':
+                                    if output_str is not None and value in output_str:
+                                        #result_dict['status'] = 'PASS'
+                                        temp['status'] = 'PASS'
+                                        #result_dict[check['id']].append({'status':'PASS'})
+                                    else:
+                                        temp['status'] = 'FAIL'
+                                        #result_dict[check['id']].append({'status':'FAIL'})
+                                elif op == "eq":
+                                    if output_str is not None and str(value).lower() == str(output_str).lower():
+                                        temp['status'] = 'PASS'
+                                        #result_dict[check['id']].append({'status':'PASS'})
+                                    else: 
+                                        temp['status'] = 'FAIL'
+                                        #result_dict[check['id']].append({'status':'FAIL'})
+                                result_dict[check['id']].append(temp)
+                                results[group_description].append(result_dict)
+                        else:
+                            output_str = get_output(check['audit'])
+                        if output_str is not None:
+                            logger.error (output_str)
+                        else:
+                            logger.error('skipped '+ str(check['id']))
+            logger.error("\n\n")
+            compresults = {}
+            total = 0
+            for k, values in results.items():
+                logger.error (k)
+                mylist = []
+                for v in values:
+                    for key, value in v.items():
+                        logger.error (key)
+                        for elmnts in value:
+                            logger.error (elmnts)
+                            mylist.append(elmnts)
+                            for x,y in elmnts.items():
+                                total+=1
+                                if x == 'status' and y == 'PASS':
+                                    count +=1
+                compresults[k] = mylist                                    
+            logger.error("\n\n")
+            logger.error("Total:{} \tScore: {}".format(total, count))
+            return render(request, 'compliance.html', { 'total': total, 'score':count,'results':compresults })
+        except yaml.YAMLError as exc:
+            logger.error(exc)
 def total_images():
     images_command = 'docker image list'
     total_images_command = 'wc -l'
